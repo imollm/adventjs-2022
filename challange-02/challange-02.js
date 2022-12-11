9a803ee63316789616ce794cbf87ad07dae51d21
Ivan Moll <ivanmoll07@gmail.com>
2022-12-11 11:17:40 +0100

    Solution challange 02

---
+++ challange-02/challange-02.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function countHours(year, holidays) {
+    let numMissingDays = 0
+
+    holidays.forEach(holidayDay => {
+        let [month, day] = holidayDay.split('/')
+        const date = new Date(`${year}/${month}/${day}`)
+        const dayOfWeak = date.getDay()
+
+        if (dayOfWeak !== 0 && dayOfWeak !== 6) {
+            numMissingDays++
+        }
+    });
+
+    return numMissingDays * 2
+}
+
+module.exports = countHours
---
+++ challange-02/challange-02.test.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const countHours = require('./challange-02')
+
+describe('Tests of challange 02', () => {
+    test('it should be 0 when no holidays', () => {
+        const year = 2022
+        const holidays = []
+
+        const recoupHours = countHours(year, holidays)
+
+        expect(recoupHours).toBe(0)
+    })
+
+    test('it should returns type number', () => {
+        const year = 2022
+        const holidays = ['01/06', '04/01', '12/25']
+
+        const recoupHours = countHours(year, holidays)
+
+        expect(typeof recoupHours).toBe('number')
+    })
+
+    test('i should be 4 hours', () => {
+        const year = 2023
+        const holidays = ['01/06', '04/01', '12/25']
+
+        const recoupHours = countHours(year, holidays)
+
+        expect(recoupHours).toBe(4)
+    })
+
+    test('it should be 4 hours', () => {
+        const year = 2022
+        const holidays = ['01/06', '04/01', '12/25']
+
+        const recoupHours = countHours(year, holidays)
+
+        expect(recoupHours).toBe(4)
+    })
+
+    test('it should be 10 hours', () => {
+        const year = 1985
+        const holidays = ['01/01', '01/06', '02/02', '02/17', '02/28', '06/03', '12/06', '12/25']
+
+        const recoupHours = countHours(year, holidays)
+
+        expect(recoupHours).toBe(10)
+    })
+
+    test('it should be 0 hours', () => {
+        const year = 2000
+        const holidays = ['01/01']
+
+        const recoupHours = countHours(year, holidays)
+
+        expect(recoupHours).toBe(0)
+    })
+})
